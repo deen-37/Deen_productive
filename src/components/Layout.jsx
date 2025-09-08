09f4f8177566311b6a431abd2ecff700e653cf02
Selu_1 <selehadin37@gmail.com>
2025-09-07 21:06:50 -0400

    added more interactivity

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -9,9 +9,9 @@ export default function Layout(props) {
 
     const footer = (
         <footer>
-            <p>Built by <a href="https://www.selehadin.netlify.app">Selehadin Abebe</a></p>
-            <p>© 2025 30-Day productivity Program. All rights reserved.</p>
+            <p>Built by Selehadin Abebe <br /> © 2025 30-Day productivity Program. All rights reserved.</p>
         </footer>
+
     )
     return (
         <>
